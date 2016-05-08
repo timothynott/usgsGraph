0c9fcc1061e6d07576dff42bfdb3d28bc9b5da68
Kylie <kyliepace@gmail.com>
2016-05-08 16:52:35 -0700

    requested data from usgs server and saved timestamp and cfs as x, y values in myData array

--- js/script.js
+++ js/script.js
@@ -1,67 +1,101 @@
+var myData = [];
+var getData = function(){
+
+	var request = {
+		bBox: "-83.000000,38.000000,-82.500000,38.500000",
+		period: "P1D",
+		parameterCD: "00060",
+		siteType:"ST",
+		format: "json",
+	};
+	$.ajax({
+		url: "http://waterservices.usgs.gov/nwis/iv/?",
+		format: "json",
+		data: request,
+		type: "GET",
+	})
+	.done(function(result){
+		var timeSeries = result.value.timeSeries[0];
+		console.log(timeSeries);
+		/*var searchResults = showSearchResults(tags, result.items.length);
+		$(".search-results").html(searchResults);*/
+		//myData = [result.timeSeries[3].values.dateTime,result.timeSeries[3].values.children().text()];
+		$.each(timeSeries.values[0].value, function(i, value){
+			
+			myData.push({x:value.dateTime, y:value.value})
+
+			
+			/*var insperation = showInspiration(item);
+			$(".results").append(insperation);*/
+		});
+		console.log(myData);
+	})
+	.fail(function(jqXHR, error){
+		/*var errorElem = showError(error);
+		$(".search-results").append(errorElem);*/
+	});
+};
+
+
+
+
+
+
 $(document).ready(function(){
-nv.addGraph(function() {
-  var chart = nv.models.lineChart()
+	getData();
+
+	nv.addGraph(function() {
+  		var chart = nv.models.lineChart()
                 .margin({left: 100, right: 50})  //Adjust chart margins to give the x-axis some breathing room.
                 .useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!
-              
                 .showLegend(false)       //Show the legend, allowing users to turn on/off line series.
                 .showYAxis(true)        //Show the y-axis
                 .showXAxis(true)        //Show the x-axis
-  ;
+  		;
 
-  chart.xAxis     //Chart x-axis settings
-      .axisLabel('Time (HH:MM)')
-     
-      .tickFormat(d3.format(',r'));
 
-  chart.yAxis     //Chart y-axis settings
-      .axisLabel('Flow (cfs)')
-      .tickFormat(d3.format('.02f'));
 
-  /* Done setting the chart up? Time to render it!*/
-  var myData = sinAndCos();   //You need data...
+  		chart.xAxis     //Chart x-axis settings
+      	.axisLabel('Time (HH:MM)')
+      	.tickFormat(d3.format(',r'));
 
-  d3.select('svg')    //Select the <svg> element you want to render the chart in.   
-      .datum(myData)         //Populate the <svg> element with chart data...
-      .call(chart);          //Finally, render the chart!
+  		chart.yAxis     //Chart y-axis settings
+      	.axisLabel('Flow (cfs)')
+      	.tickFormat(d3.format('.02f'));
 
-  //Update the chart when window resizes.
-  nv.utils.windowResize(function() { chart.update() });
-  return chart;
-});
-/**************************************
- * Simple test data generator
- */
-function sinAndCos() {
-  var sin = [],sin2 = [],
-      cos = [];
+  		/* Done setting the chart up? Time to render it!*/
+  		//var myData = sinAndCos();   //You need data...
 
-  //Data is represented as an array of {x,y} pairs.
-  for (var i = 0; i < 100; i++) {
-    sin.push({x: i, y: Math.sin(i/10)});
-    sin2.push({x: i, y: Math.sin(i/10) *0.25 + 0.5});
-    cos.push({x: i, y: .5 * Math.cos(i/10)});
-  }
+  		d3.select('svg')    //Select the <svg> element you want to render the chart in.   
+      	.datum(myData)         //Populate the <svg> element with chart data...
+      	.call(chart);          //Finally, render the chart!
 
-  //Line chart data should be sent as an array of series objects.
-  return [
-    {
-      values: sin,      //values - represents the array of {x,y} data points
-      key: 'Sine Wave', //key  - the name of the series.
-      color: '#0F5498'  //color - optional: choose your own line color.
-    },
-    /*{
-      values: cos,
-      key: 'Cosine Wave',
-      color: '#2ca02c'
-    },
-    {
-      values: sin2,
-      key: 'Another sine wave',
-      color: '#7777ff',
-      area: true      //area - set to true if you want this line to turn into a filled area chart.
-    }*/
-  ];
-}
+  		//Update the chart when window resizes.
+  		nv.utils.windowResize(function() { chart.update() });
+  	return chart;
+	});
+	/**************************************
+ 		* Simple test data generator
+ 	*/
+	function sinAndCos() {
+  		var sin = [],sin2 = [],
+      	cos = [];
+
+  		//Data is represented as an array of {x,y} pairs.
+  		for (var i = 0; i < 100; i++) {
+    		sin.push({x: i, y: Math.sin(i/10)});
+    		
+  		}
+
+  		//Line chart data should be sent as an array of series objects.
+  		return [
+    		{
+      		values: sin,      //values - represents the array of {x,y} data points
+      		key: 'cfs', //key  - the name of the series.
+      		color: '#0F5498'  //color - optional: choose your own line color.
+    		},
+    
+  		];
+	}
 
 });
