1ba02f4c83aefc825e728d989c5ec3f57a054b3a
Kylie <kyliepace@gmail.com>
2016-05-08 17:09:26 -0700

    passing usgs data into grapher

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var myData = [];
+var flowSeries = [];
 var getData = function(){
 
 	var request = {
@@ -16,33 +16,32 @@ var getData = function(){
 	})
 	.done(function(result){
 		var timeSeries = result.value.timeSeries[0];
-		console.log(timeSeries);
 		/*var searchResults = showSearchResults(tags, result.items.length);
 		$(".search-results").html(searchResults);*/
 		//myData = [result.timeSeries[3].values.dateTime,result.timeSeries[3].values.children().text()];
 		$.each(timeSeries.values[0].value, function(i, value){
-			
-			myData.push({x:value.dateTime, y:value.value})
-
-			
+			flowSeries.push({x:value.dateTime, y:value.value})
 			/*var insperation = showInspiration(item);
 			$(".results").append(insperation);*/
 		});
-		console.log(myData);
+		console.log(flowSeries);
 	})
 	.fail(function(jqXHR, error){
 		/*var errorElem = showError(error);
 		$(".search-results").append(errorElem);*/
 	});
+	return [
+    		{
+      		values: flowSeries,      //values - represents the array of {x,y} data points
+      		key: 'cfs', //key  - the name of the series.
+      		color: '#0F5498'  //color - optional: choose your own line color.
+    		},
+    
+  		];
 };
 
-
-
-
-
-
+///////////////////////////////ON LOAD////////////////////////////////
 $(document).ready(function(){
-	getData();
 
 	nv.addGraph(function() {
   		var chart = nv.models.lineChart()
@@ -53,8 +52,6 @@ $(document).ready(function(){
                 .showXAxis(true)        //Show the x-axis
   		;
 
-
-
   		chart.xAxis     //Chart x-axis settings
       	.axisLabel('Time (HH:MM)')
       	.tickFormat(d3.format(',r'));
@@ -63,8 +60,8 @@ $(document).ready(function(){
       	.axisLabel('Flow (cfs)')
       	.tickFormat(d3.format('.02f'));
 
-  		/* Done setting the chart up? Time to render it!*/
-  		//var myData = sinAndCos();   //You need data...
+  		//Done setting the chart up? Time to render it!*/
+  		var myData = getData();   //You need data...
 
   		d3.select('svg')    //Select the <svg> element you want to render the chart in.   
       	.datum(myData)         //Populate the <svg> element with chart data...
@@ -76,17 +73,16 @@ $(document).ready(function(){
 	});
 	/**************************************
  		* Simple test data generator
- 	*/
+ 	
 	function sinAndCos() {
-  		var sin = [],sin2 = [],
-      	cos = [];
+  		var sin = []
 
   		//Data is represented as an array of {x,y} pairs.
   		for (var i = 0; i < 100; i++) {
     		sin.push({x: i, y: Math.sin(i/10)});
     		
   		}
-
+  		console.log(sin);
   		//Line chart data should be sent as an array of series objects.
   		return [
     		{
@@ -96,6 +92,6 @@ $(document).ready(function(){
     		},
     
   		];
-	}
+	}*/
 
 });
