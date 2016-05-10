9eb540e471aa06885d78d3e16bcbfa0d14de0545
Kylie <kyliepace@gmail.com>
2016-05-10 15:56:34 -0700

    added options to flowSeries
    http://www.chartjs.org/docs/#scales

--- js/script.js
+++ js/script.js
@@ -4,9 +4,53 @@ var flowSeries = {
 	datasets:[
 			{
 			strokeColor: black,
-			data:[]
+			data:[],
+			xAxisID: "Time",
+			yAxisID: "Flow (cfs)",
+			fill:false,
+			lineTension: 0,
+			borderColor: white,
+			pointRadius: 0,
+			
 			}
-		]
+		],
+	options: {
+		scales:{
+			xAxes: [{
+				type: "time",
+				gridLines.display: false,
+				ticks.display: true
+			}],
+			time: {
+        			// string/callback - By default, date objects are expected. You may use a pattern string from http://momentjs.com/docs/#/parsing/string-format/ to parse a time string format, or use a callback function that is passed the label, and must return a moment() instance.
+        			parser: false,
+        			// string - By default, unit will automatically be detected.  Override with 'week', 'month', 'year', etc. (see supported time measurements)
+        			unit: false,
+
+        			// Number - The number of steps of the above unit between ticks
+        			unitStepSize: 1
+
+        			// string - By default, no rounding is applied.  To round, set to a supported time unit eg. 'week', 'month', 'year', etc.
+        			round: false,
+
+        			// Moment js for each of the units. Replaces `displayFormat`
+        			// To override, use a pattern string from http://momentjs.com/docs/#/displaying/format/
+        			displayFormats: {
+            				'millisecond': 'SSS [ms]',
+            				'second': 'h:mm:ss a', // 11:20:01 AM
+            				'minute': 'h:mm:ss a', // 11:20:01 AM
+            				'hour': 'MMM D, hA', // Sept 4, 5PM
+            				'day': 'll', // Sep 4 2015
+            				'week': 'll', // Week 46, or maybe "[W]WW - YYYY" ?
+            				'month': 'MMM YYYY', // Sept 2015
+            				'quarter': '[Q]Q - YYYY', // Q3
+            				'year': 'YYYY', // 2015
+        			},
+        			// Sets the display format used in tooltip generation
+        			tooltipFormat: ''
+    			},
+		}
+	}
 }
 var gageName = "";
 var getData = function(){
