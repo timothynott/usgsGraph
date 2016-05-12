f31db46d7dd6d53da82be3c8d8d27a59e5bbb538
Kylie <kyliepace@gmail.com>
2016-05-11 19:56:46 -0700

    title and number of results works

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
  var yData = [];
  var xData = [];
+ var gageName = [];
  var flowSeries = {
       labels:xData,
       datasets:[{
-          //label: gageName,
+          label: gageName,
           pointStrokeColor: "#fff",
           strokeColor: "rgba(220,220,220,1)",
           data:yData
@@ -19,7 +20,6 @@
   
 var options = {scaleShowGridLines : true, scaleShowVerticalLines: true};
   
-var gageName = "";
 var getData = function(){
   var request = {
     bBox: "-83.000000,38.000000,-82.500000,38.500000",
@@ -35,16 +35,15 @@ var getData = function(){
     type: "GET",
   })
   .done(function(result){
-    var siteData = result.value.timeSeries[0];
-    var gageName = siteData.sourceInfo.siteName;
-
+    
+    gageName.push(result.value.timeSeries[0].sourceInfo.siteName);
+    $(".graph h5").html("1 of "+result.value.timeSeries.length+"gages near you");
     /*var searchResults = showSearchResults(tags, result.items.length);
     $(".search-results").html(searchResults);*/
     //myData = [result.timeSeries[3].values.dateTime,result.timeSeries[3].values.children().text()];
     $.each(result.value.timeSeries[0].values[0].value, function(i, value){
       xData.push(value.dateTime);
       yData.push(parseInt(value.value));
-      
       //.slice(11,16)
       //.replace(":","")
       /*var insperation = showInspiration(item);
@@ -53,7 +52,6 @@ var getData = function(){
     var hydrograph = document.getElementById('graph').getContext('2d');
   var myChart = new Chart(hydrograph,{
     type: "line",
-    //borderWidth: 1px,
     data: flowSeries
   });
   })
@@ -63,18 +61,13 @@ var getData = function(){
   })
 };
 
-var showData = function(gageName){
-  //flowSeries.datasets.label.val(gageName);
-};
-
-
 
 
 ///////////////////////////////ON LOAD////////////////////////////////
 $(document).ready(function(){
 
   getData();
-  showData();
+  //showData();
   
 
   
