73e12cec3ae9a4b60fdac827bf51decba1a75e02
Kylie <kyliepace@gmail.com>
2016-05-11 21:47:23 -0700

    gets data from browser coordinates but having trouble with scope of coordinate variables

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+
+ var getLocation = function() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position){
+          long=position.coords.longitude.toString().slice(0,11);
+          lat=position.coords.latitude.toString().slice(0,9);
+          longExt=(position.coords.longitude+1).toString().slice(0,11);
+          latExt=(position.coords.latitude+1).toString().slice(0,9);
+          //have to put getData() function here so that long, lat coord values are remembered
+           getData(long, lat, longExt, latExt);
+        });
+        
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+};
+
+var resetData = function(){
+  yData=[];
+  xData=[];
+  gageName=[];
+};
+
  var yData = [];
  var xData = [];
  var gageName = [];
@@ -20,12 +43,13 @@
   
 var options = {scaleShowGridLines : true, scaleShowVerticalLines: true};
   
-var getData = function(){
+var getData = function(long, lat, longExt, latExt){
   var request = {
-    bBox: "-83.000000,38.000000,-82.500000,38.500000",
+    bBox: long+","+lat+","+longExt+","+latExt,
     period: "P1D",
     parameterCD: "00060",
     siteType:"ST",
+    siteStatus: "active",
     format: "json",
   }
   $.ajax({
@@ -35,10 +59,11 @@ var getData = function(){
     type: "GET",
   })
   .done(function(result){
-    $(".graph h5").html("1 of "+result.value.timeSeries.length+"gages near you");
-    console.log(result);
+    $(".graph h5").html("1 of "+result.value.timeSeries.length+" gages near you");
+    numberOfTimeSeries = result.value.timeSeries.length;
     gageName.push(result.value.timeSeries[i].sourceInfo.siteName);
     console.log(gageName);
+      //go through each x,y pair in the specified timeSeries
       $.each(result.value.timeSeries[i].values[0].value, function(i, value){
         xData.push(value.dateTime);
         yData.push(parseInt(value.value));
@@ -56,18 +81,24 @@ var getData = function(){
   })
 };
 
-
-
 ///////////////////////////////ON LOAD////////////////////////////////
 $(document).ready(function(){
+
+  getLocation();
+  //getData();
   i = 0;
-  getData();
+ 
+  
   $(".main").on("click","#leftArrow", function(){
-      i --;
-      getData();
+    //click on arrow to reduce value of i by one
+    i --;
+    resetData();
+    getData();
+      
   });
   $(".main").on("click", "#rightArrow", function(){
     i ++;
+    resetData();
     getData();
   })
 });
