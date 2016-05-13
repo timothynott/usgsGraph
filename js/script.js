2a6cab2e185df50e3abcdb9e896141bd3b794b34
Kylie <kyliepace@gmail.com>
2016-05-12 19:21:56 -0700

    call writeRequest from getLocation so that position variables can be accessed outside of getLocation

--- js/script.js
+++ js/script.js
@@ -3,33 +3,46 @@ var lat = "";
 var longExt="";
 var latExt = "";
 var n = 0;
- 
- var getLocation = function() {
-    if (navigator.geolocation) {
-      //anonymous function
-        navigator.geolocation.getCurrentPosition(function(position){
-        long=position.coords.longitude.toString().slice(0,11);
-        lat=position.coords.latitude.toString().slice(0,9);
-        longExt=(position.coords.longitude+1).toString().slice(0,11);
-        latExt=(position.coords.latitude+1).toString().slice(0,9);
-        });
+
+
+//put the coordinates (received in function described one below) into a request 
+//that will be sent to USGS 
+var writeRequest = function(position){
+  long = position.coords.longitude.toString().slice(0,11);
+  lat=position.coords.latitude.toString().slice(0,9);
+  longExt=(position.coords.longitude+1).toString().slice(0,11);
+  latExt=(position.coords.latitude+1).toString().slice(0,9);  
+  var request = {
+    bBox: long+","+lat+","+longExt+","+latExt,
+    period: "P1D",
+    parameterCD: "00060",
+    siteType:"ST",
+    siteStatus: "active",
+    format: "json",
+  }
+  console.log(request);
+  sendRequest(request);
+};
+//get coordinates from browser by calling writeRequest function from getCurrentPosition
+//putting writeRequest as a callback allows the position values to be remembered
+var getLocation = function() {
+  if (navigator.geolocation) {
+    //anonymous function
+    navigator.geolocation.getCurrentPosition(writeRequest);
+  }
+  else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+};
+
+
         
-          //have to put getData() function here so that long, lat coord values are remembered
-          
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-};
 
-var resetData = function(){
-  yData=[];
-  xData=[];
-  gageName=[];
-};
-
+//define data arrays
  var yData = [];
  var xData = [];
  var gageName = [];
+//flowSeries is the data object that is populated from USGS json
  var flowSeries = {
       labels:xData,
       datasets:[{
@@ -46,71 +59,75 @@ var resetData = function(){
       }],
       options: options
   };
-  
+//include options for flowSeries
 var options = {scaleShowGridLines : true, scaleShowVerticalLines: true};
-  
-var getData = function(lng, lt, xlng, xlt){
-  long = (lng)?lng:long;
-  lat = (lt)?lt:lat;
-  longExt = (xlng)?xlng:longExt;
-  latExt = (xlt)?xlt:latExt;
-  
-  var request = {
-    bBox: lng+","+lt+","+xlng+","+xlt,
-    period: "P1D",
-    parameterCD: "00060",
-    siteType:"ST",
-    siteStatus: "active",
-    format: "json",
-  }
+
+
+
+
+//if the request (one funtion down) is successfully sent, populate flowSeries with the results
+var populateSeries = function(result){
+  //show how many results
+  $(".graph h5").html("1 of "+result.value.timeSeries.length+" gages near you");
+  //show the name of the result
+  gageName=result.value.timeSeries[n].sourceInfo.siteName;
+  console.log(gageName);
+  //go through each x,y pair in the result. value of n starts at 0 and changes as arrows are clicked
+  $.each(result.value.timeSeries[n].values[0].value, function(i, value){
+    xData.push(value.dateTime);
+    yData.push(parseInt(value.value));
+  })
+  var hydrograph = document.getElementById('graph').getContext('2d');
+  var myChart = new Chart(hydrograph,{
+    type: "line",
+    data: flowSeries  
+  });
+};
+
+//send the request to USGS
+var sendRequest = function(request){
   $.ajax({
     url: "http://waterservices.usgs.gov/nwis/iv/?",
     format: "json",
     data: request,
     type: "GET",
   })
-  .done(function(result){
-    $(".graph h5").html("1 of "+result.value.timeSeries.length+" gages near you");
-    numberOfTimeSeries = result.value.timeSeries.length;
-    gageName.push(result.value.timeSeries[n].sourceInfo.siteName);
-    console.log(gageName);
-      //go through each x,y pair in the specified timeSeries
-      $.each(result.value.timeSeries[n].values[0].value, function(i, value){
-        xData.push(value.dateTime);
-        yData.push(parseInt(value.value));
-      })
-    var hydrograph = document.getElementById('graph').getContext('2d');
-    var myChart = new Chart(hydrograph,{
-          type: "line",
-          data: flowSeries  
-      });
-    
-  })
+  //if json request works, call populateSeries() function so result variable
+  //will be accessible
+  .done(populateSeries)
   .fail(function(jqXHR, error){
     /*var errorElem = showError(error);
     $(".search-results").append(errorElem);*/
   })
 };
 
+
+//clear xData, yData, and gageName arrays
+var resetData = function(){
+  yData=[];
+  xData=[];
+  gageName=[];
+};
+
 ///////////////////////////////ON LOAD////////////////////////////////
 $(document).ready(function(){
 
   getLocation();
-  //getData();
   n = 0;
-  getData(lng, lt, xlng, xlt);
+  //writeRequest() calls sendRequest();
+  //sendRequest already calls populateSeries();
   
   $(".main").on("click","#leftArrow", function(){
     //click on arrow to reduce value of i by one
     n --;
     resetData();
-    getData(long, lat, longExt, latExt);
+    populateSeries();
       
   });
   $(".main").on("click", "#rightArrow", function(){
     n ++;
     resetData();
-    getData(long, lat, longExt, latExt);
+    populateSeries();
   });
 });
 
